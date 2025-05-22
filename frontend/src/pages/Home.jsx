c9d9a2f8039c4597b79f219f129a98d7256ce6b0
cBasique <maelroulette12@gmail.com>
2025-05-22 16:27:18 +0200

    Modif home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <div className="home">
       <h1>{t("home.title")}</h1>
-      <p>This is the main page of the application.</p>
     </div>
   );
 }
