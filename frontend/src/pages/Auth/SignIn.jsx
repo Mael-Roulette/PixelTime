6ded38bf91c510598c6582957755255ac30a7e5c
Floriane <florianelouis49@gmail.com>
2025-05-27 16:35:52 +0200

    Connexion

--- frontend/src/pages/Auth/SignIn.jsx
+++ frontend/src/pages/Auth/SignIn.jsx
@@ -27,73 +27,77 @@ export default function SingIn() {
           <h1>{t("signup.title")}</h1>
           <form>
             <div className="form-group">
-              <label htmlFor="pseudo">
-                Pseudo*
-                <input type="text" name="pseudo"/>
-              </label>
-              <label htmlFor="email">
-                Email*
-                <input type="email" name="email" placeholder="email@email.com" />
-              </label>
-              <label htmlFor="password">
-                {t("signup.password")}*
-                <div style={{ position: 'relative' }}>
-                  <input
-                    type={type}
-                    name="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    autoComplete="current-password"
-                  />
-                  <span
-                    className="password-toggle"
-                    onClick={handleToggle}
-                    style={{
-                      position: 'absolute',
-                      right: '10px',
-                      top: '50%',
-                      transform: 'translateY(-50%)',
-                      cursor: 'pointer',
-                      color: "#293858"
-                    }}
-                  >
-                    {showPassword ? <FiEye size={20} /> : <FiEyeOff size={20} />}
-                  </span>
-                </div>
-              </label>
-              <label htmlFor="confirm-password">
-                {t("signup.confirmPassword")}*
-                <div style={{ position: 'relative' }}>
-                  <input
-                    type={type}
-                    name="confirm-password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    autoComplete="current-password"
-                  />
-                  <span
-                    className="password-toggle"
-                    onClick={handleToggle}
-                    style={{
-                      position: 'absolute',
-                      right: '10px',
-                      top: '50%',
-                      transform: 'translateY(-50%)',
-                      cursor: 'pointer',
-                      color: "#293858"
-                    }}
-                  >
-                    {showPassword ? <FiEye size={20} /> : <FiEyeOff size={20} />}
-                  </span>
-                </div>
-              </label>
-              <p>*{t("signup.fieldsRequired")}</p>
+              <div className="form-group-info">
+                <label htmlFor="pseudo">
+                  Pseudo*
+                  <input type="text" name="pseudo" />
+                </label>
+                <label htmlFor="email">
+                  Email*
+                  <input type="email" name="email" placeholder="email@email.com" />
+                </label>
+              </div>
+              <div className="form-group-password">
+                <label htmlFor="password">
+                  {t("signup.password")}*
+                  <div style={{ position: 'relative' }}>
+                    <input
+                      type={type}
+                      name="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      autoComplete="current-password"
+                    />
+                    <span
+                      className="password-toggle"
+                      onClick={handleToggle}
+                      style={{
+                        position: 'absolute',
+                        right: '10px',
+                        top: '50%',
+                        transform: 'translateY(-50%)',
+                        cursor: 'pointer',
+                        color: "#293858"
+                      }}
+                    >
+                      {showPassword ? <FiEye size={20} /> : <FiEyeOff size={20} />}
+                    </span>
+                  </div>
+                </label>
+                <label htmlFor="confirm-password">
+                  {t("signup.confirmPassword")}*
+                  <div style={{ position: 'relative' }}>
+                    <input
+                      type={type}
+                      name="confirm-password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      autoComplete="current-password"
+                    />
+                    <span
+                      className="password-toggle"
+                      onClick={handleToggle}
+                      style={{
+                        position: 'absolute',
+                        right: '10px',
+                        top: '50%',
+                        transform: 'translateY(-50%)',
+                        cursor: 'pointer',
+                        color: "#293858"
+                      }}
+                    >
+                      {showPassword ? <FiEye size={20} /> : <FiEyeOff size={20} />}
+                    </span>
+                  </div>
+                </label>
+              </div>
+              <p className="form-group-required">*{t("signup.fieldsRequired")}</p>
             </div>
-            <input type="submit" value={t("signup.continue")} className="button-primary" />
+            <input type="submit" value={t("signup.continue")} className="submit-button button-primary" />
           </form>
           <p>
             {t("signup.alreadyHaveAccount")}
-            <a href="/inscription"> {t("signup.login")}</a>
+            <a href="/connexion"> {t("signup.login")}</a>
           </p>
         </div >
       </main >
